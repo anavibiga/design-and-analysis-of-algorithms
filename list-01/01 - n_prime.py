1bf60c23bfcc4b239714941935b9460976d47ef8
anavibiga <gabrielavianadearaujo@gmail.com>
2023-10-13 15:54:46 -0300

    update list 1

---
+++ list-01/01 - n_prime.py
@@ -0,0 +1,16 @@
+# Exercicio 01 - n (>1) primeiros primos dado um valor n (>0) dado pelo usuário
+
+num = int(input('Diga o número inicial: '))
+
+def primo(n):
+    c = 1
+    cdiv = 0
+    while (c<=n):
+        if (n%c == 0):
+            cdiv += 1
+        c += 1    
+    if (cdiv == 2):
+        print('{} é primo.'.format(n))
+
+for i in range(num,0,-1):
+    primo(i)
