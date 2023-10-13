9eb970cbcd964cd62eb65236e1f37f24279c2bba
anavibiga <gabrielavianadearaujo@gmail.com>
2023-10-13 16:49:31 -0300

    update list

---
+++ list-01/02 - read_txt.py
@@ -0,0 +1,13 @@
+with open("texto.txt") as arquivo:
+    texto = arquivo.read()
+
+vogais = ['a', 'e', 'i', 'o', 'u']
+
+c = 0
+
+for i in texto:
+    if i in vogais:
+        c += 1
+
+print('O texto é:\n{}'.format(texto))    
+print('\nO total de vogais no texto é: {}.'.format(c))
