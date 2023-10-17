520af47c99447f77e99d9a585d971d832d5d0db6
anavibiga <gabrielavianadearaujo@gmail.com>
2023-10-17 00:45:19 -0300

    update 04

--- list-01/04 - binary_search.py
+++ list-01/04 - binary_search.py
@@ -25,5 +25,32 @@ def binary_search(list, item):
 
 binary_search(list, item)
 
+# Binary Search recursivo
+
+item = int(input('Qual número você quer encontrar? '))
+
+list = [1, 3, 5, 7, 9]
+
+def binary_search(list, item, low, high):
+    if low <= high:
+        mid = (low + high) // 2
+        guess = list[mid]
+
+        if guess == item:
+            return mid
+        elif guess > item:
+            return binary_search(list, item, low, mid - 1)
+        else:
+            return binary_search(list, item, mid + 1, high)
+    else:
+        return None
+
+result = binary_search(list, item, 0, len(list) - 1)
+
+if result is not None:
+    print('O item {} foi encontrado na posição {}.'.format(item, result))
+else:
+    print('O item {} não consta na lista.'.format(item))
+
 
 
